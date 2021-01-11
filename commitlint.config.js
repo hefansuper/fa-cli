83770bdc84b5a36a3acbe1c9ace713859af0d48c
stephenHe <hefan@ichangtou.com>
2021-01-11 17:21:14 +0800

    feat(package.json): commitlint
    commit 增加检验
    
    BREAKING CHANGE: 如果commit不符合angular的规范的话，会打断提交的进程

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: stephenHe
+ * @Date: 2021-01-11 17:03:49
+ * @LastEditors: stephenHe
+ * @LastEditTime: 2021-01-11 17:05:45
+ * @Description: commitlint的配置文件，主要来处理提交时候的校验。
+ * @FilePath: /my-cli-app/commitlint.config.js
+ */
+
+
+module.exports = {
+    extends: ['@commitlint/config-conventional']
+};
