3772269f5448d3b99db0254828eb23e5a8ac27d6
stephenHe <hefan@ichangtou.com>
2021-01-12 11:40:05 +0800

    feat(ggg): hhhhhh

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,19 +3,7 @@ import { useLocation } from "react-router-dom";
 function Home() {
   console.log(useLocation());
 
-
-
-
-
-
-
-
-
-
-
-
-
-  
+  let a = 0;
   return <div className="Home">home</div>;
 }
 export default React.memo(Home);
