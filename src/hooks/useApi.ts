9289cede89e2838ddf1a58aa3ad771e3dcc4b89a
stephenhe <hf597833184@163.com>
2021-03-02 16:28:44 +0800

    feat(useapi.ts): useApi hook
    完成useApi的hook

--- src/hooks/useApi.ts
+++ src/hooks/useApi.ts
@@ -2,39 +2,100 @@
  * @Author: stephenHe
  * @Date: 2021-03-01 20:52:12
  * @LastEditors: stephenHe
- * @LastEditTime: 2021-03-01 21:06:31
+ * @LastEditTime: 2021-03-02 16:27:31
  * @Description: 请求方法的封装。 使用hooks的方式。
  * @FilePath: /my-cli-app/src/hooks/useApi.ts
  */
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useReducer } from 'react'
 import axios from 'axios'
 
+interface IUseApiProps {
+  initialUrl: string;
+  initialData: IData;
+}
 
-export default function useApi(initialUrl: string) {
+enum Type {
+  'FETCH_INIT',
+  'FETCH_SUCCESS',
+  'FETCH_FAILURE'
+}
+
+interface IData {
+  [key: string]: string
+}
+
+
+interface IState {
+  isLoading: boolean;
+  isError: boolean;
+  data?: IData;
+}
+
+interface IAction {
+  type: 'FETCH_INIT' | 'FETCH_SUCCESS' | 'FETCH_FAILURE';
+  payload?: IData;
+}
+
+const dataFetchReducer = (state: IState, action: IAction): IState => {
+  switch (action.type) {
+    case 'FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false
+      }
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload
+      }
+    case 'FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true
+      }
+    default:
+      return { ...state }
+  }
+
+}
+
+export default function useApi({ initialUrl, initialData }: IUseApiProps) {
   const [url, setUrl] = useState<string>(initialUrl)
-  const [data, setData] = useState<Object>({})
-  const [isError, seIsError] = useState<boolean>(false)
-  const [isLoading, seIsLoading] = useState<boolean>(true)
+  const [state, dispatch] = useReducer(dataFetchReducer, {
+    isError: false,
+    isLoading: false,
+    data: initialData
+  });
 
   useEffect(() => {
+    let didCancel = false;
 
     const fetchData = async () => {
+      dispatch({ type: 'FETCH_INIT' });
+
       try {
         const result = await axios(url)
-        setData(result)
+
+        !didCancel && dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
       } catch (error) {
-        seIsError(true)
-      } finally {
-        seIsLoading(false)
+        !didCancel && dispatch({ type: 'FETCH_FAILURE' });
       }
     }
 
-    fetchData()
+    fetchData();
+
+    return () => {
+      didCancel = true;
+    };
   }, [url])
 
 
-  return [{ data, isError, isLoading }, setUrl]
+  return [state, setUrl]
 }
 
 
